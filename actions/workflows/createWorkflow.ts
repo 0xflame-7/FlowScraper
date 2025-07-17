a94579bfb3e1d853aa9c3103d5b4dd27ade51f90
0xflame-7 <0xflame.7@gmail.com>
2025-07-17 17:41:29 +0530

    LOG: 7-17-25

---
+++ actions/workflows/createWorkflow.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import {
+  createWorkflowSchema,
+  createWorkflowSchemaType,
+} from "@/schema/workflow";
+import { auth } from "@clerk/nextjs/server";
+
+export async function CreateWorkflow(form: createWorkflowSchemaType) {
+  const { success, data } = createWorkflowSchema.safeParse(form);
+
+  if (!success) {
+    throw new Error("invalid from data");
+  }
+
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("auth");
+  }
+}
